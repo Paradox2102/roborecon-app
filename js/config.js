941c88d7a48b42fd77ed83a85cafd6520329dee8
runninbear5 <blakelieber@gmail.com>
2018-02-18 12:11:49 -0800

    Edits to config file to update to this year game

--- js/config.js
+++ js/config.js
@@ -4,11 +4,10 @@
 var tba_api_ranking_config = [
   { id: 'ranking', title: 'Rank', display_order: 1, key: 'rank' },
   { id: 'rankingScore', title: 'Ranking Score', display_order: 2, arr_index: 0 },
-  { id: 'rankingMatchPoints', title: 'Match Points', display_order: 3, arr_index: 1 },
+  { id: 'rankingParkClimbPoints', title: 'Park/Climb Points', display_order: 3, arr_index: 1 },
   { id: 'rankingAuto', title: 'Auto', display_order: 4, arr_index: 2 },
-  { id: 'rankingRotor', title: 'Rotor', display_order: 5, arr_index: 3 },
-  { id: 'rankingTouchpad', title: 'Touchpad', display_order: 6, arr_index: 4 },
-  { id: 'Pressure', title: 'Pressure', display_order: 7, arr_index: 5 },
+  { id: 'rankingOwnership', title: 'Ownership', display_order: 5, arr_index: 3 },
+  { id: 'rankingVault', title: 'Vault', display_order: 6, arr_index: 4 },
   { id: 'rankingRecord', title: 'Record (W-L-T)', display_order: 8, key: 'record'},
   { id: 'rankingPlayed', title: 'Played', display_order: 9, key: 'matches_played' }
 ];
@@ -35,16 +34,16 @@ var tba_api_scoring_config = [
     dtype: 'int',
     subtraction: ['vaultBoostTotal', 'vaultBoostPlayed']
   }, {
-    id: 'xTotalLowFuels',
-    title: 'Low Fuels',
+    id: 'xLeftOverLevitateCubes',
+    title: 'Unused Levitate Cubes',
     dtype: 'int',
-    agg: ['autoFuelLow', 'teleopFuelLow']
+    subtraction: ['vaultLevitateTotal', 'vaultLevitatePlayed']
   }];
   // { keeping this in here so we remember we can do substitutions
   //   id: 'position2',
   //   title: 'Position Two',
   //   dtype: 'int',
-  //   sub: 'position2crossings'
+  //   substitution: 'position2crossings'
   // }
 
 
@@ -73,23 +72,26 @@ var app_dashboard_config = {
     { title: 'TOTAL POINTS', data: 'totalPoints', orderSequence: ['desc', 'asc'] },
     { title: 'TELEOP Points', data: 'teleopPoints', orderSequence: ['desc', 'asc'], visible: false },
     { title: 'AUTO Points', data: 'autoPoints', orderSequence: ['desc', 'asc'], visible: false },
-    { data: 'xTotalRotorPoints', title: 'ROTOR Points', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { data: 'xTotalFuelPoints', title: 'FUEL Points', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'TAKEOFF Points', data: 'teleopTakeoffPoints', orderSequence: ['desc', 'asc'] },
-    { title: 'MOBILITY Points (auto)', data: 'autoMobilityPoints', orderSequence: ['desc', 'asc'] },
-    // counts
-    { data: 'xTotalRotorsEngaged', title: '# Rotors Engaged', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { data: 'xTotalHighFuels', title: '# High Goals', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { data: 'xTotalLowFuels', title: '# Low Goals', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
-    // rotor/fuel details
-    { title: 'Auto Rotor Points', data: 'autoRotorPoints', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'Teleop Rotor Points', data: 'teleopRotorPoints', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'Auto Fuel Points', data: 'autoFuelPoints', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'Teleop Fuel Points', data: 'teleopFuelPoints', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'Auto Fuel High', data: 'autoFuelHigh', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'Teleop Fuel High', data: 'teleopFuelHigh', orderSequence: ['desc', 'asc'], defaultContent: 0 },
-    { title: 'Auto Fuel Low', data: 'autoFuelLow', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
-    { title: 'Teleop Fuel Low', data: 'teleopFuelLow', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'SWITCH Seconds', data: 'xTotalSwitchOwnershipSec', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'SCALE Seconds', data: 'xTotalScaleOwnershipSec', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'ENDGAME Points', data: 'endgamePoints', orderSequence: ['desc', 'asc'] },
+    { title: 'CROSSING Points (auto)', data: 'autoRunPoints', orderSequence: ['desc', 'asc'] },
+    // Switch/Scale details
+    { title: 'Auto Switch Seconds', data: 'autoSwitchOwnershipSec', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Teleop Switch Seconds', data: 'teleopSwitchOwnershipSec', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Switch Boost Seconds', data: 'teleopSwitchBoostSec', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Switch Force Seconds', data: 'teleopSwitchForceSec', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Auto Scale Seconds', data: 'autoScaleOwnershipSec', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Teleop Scale Seconds', data: 'teleopScaleOwnershipSec', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Scale Boost Seconds', data: 'teleopScaleBoostSec', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Scale Force Seconds', data: 'teleopScaleForceSec', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Vault Total', data: 'vaultPoints', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Levitate Total', data: 'vaultLevitateTotal', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Levitate Played', data: 'vaultLevitatePlayed', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Boost Total', data: 'vaultBoostTotal', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Boost Played', data: 'vaultBoostPlayed', orderSequence: ['desc', 'asc'], defaultContent: 0 },
+    { title: 'Force Total', data: 'vaultForceTotal', orderSequence: ['desc', 'asc'], defaultContent: 0, visible: false },
+    { title: 'Force Played', data: 'vaultForcePlayed', orderSequence: ['desc', 'asc'], defaultContent: 0 },
     // other ranking info
     { title: 'Rotor Ranking Points', data: 'rotorRankingPointAchieved', orderSequence: ['desc', 'asc'] },
     { title: 'Rotor Bonus Points', data: 'rotorBonusPoints', orderSequence: ['desc', 'asc'] },
